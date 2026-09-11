26599d644b19440c76ea089b98f4b319cc30767f
karolnarvaez761-droid <karolnarvaez761@gmail.com>
2026-09-11 12:11:09 -0500

    hola

---
+++ arreglos.js
@@ -0,0 +1,13 @@
+let lista=[]
+    lista.push("a")
+    console.log(lista)
+    lista.push("b")
+    console.log(lista)
+
+    lista.unshift("c")
+    console.log(lista)
+
+    lista.pop()
+    console.log(lista)
+
+    lista.shift()
--- index.js
+++ index.js
@@ -1,2 +1,64 @@
-console.log("Karol Narvaez")
-console.log("Lizeth")
+class Animal {
+    nombre
+    raza
+    especie
+    edad
+    constructor(nombre, raza, especie, edad) {
+        this.nombre = nombre
+        this.raza = raza
+        this.especie = especie
+        this.edad = edad
+    }
+    registrarNombre(nuevonombre) {
+        this.nombre = nuevonombre
+    }
+    registrarRaza(nuevaRaza) {
+        this.nombre = nuevaRaza
+    }
+    registrarRaza(nuevaEspecie) {
+        this.nombre = nuevaEspecie
+    }
+    registrarRaza(nuevaEdad) {
+        this.nombre = nuevaEdad
+    }
+    mostrarInformacion() {
+        console.log(this.nombre + this.raza)
+    }
+}
+
+
+class Propietario {
+    nombrePropietario
+    Direccion
+    telefono
+    constructor(nombre, direccion, telefono) {
+        this.nombrePropietario=nombre
+        this.direccion = direccion
+        this.telefono = telefono
+        this.mascota = []
+    }
+    registrarNombre(nuevoNombre) {
+        this.nombrePropietario = nuevoNombre
+    }
+
+    resgistrarDireccion(nevaDirreccion) {
+        this.direccion = nevaDirreccion
+    }
+    registrarTelefono(nuevoTelefono) {
+        this.telefono = nuevoTelefono
+    }
+    resgistrarMascota(nuevamascota) {
+        //pop, unshift, shift push
+        this.mascota.push(nuevamascota)
+    }
+    mostrarInformacion(){
+        console.log("mascota"+this.mascota[0].nombre+"Propietario"+this.nombrePropietario)
+    }
+}
+
+let colmillo=new Animal("colmillo","pitbull","perro","12")
+colmillo.mostrarInformacion()
+
+let alexa=new Propietario("Alexa","123","1111")
+alexa.resgistrarMascota(colmillo)
+alexa.mostrarInformacion()
